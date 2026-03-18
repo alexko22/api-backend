2a117c84b51ebffcd5267d874df482356c9bdc5c
Alex Olson <alexko22222@gmail.com>
2026-03-18 16:21:58 -0400

    rest of error codes in place... debugging duplicate code issue

--- controllers/authController.js
+++ controllers/authController.js
@@ -29,7 +29,7 @@ const registerUser = async (req, res) => {
   // third potential error: user with said email already exists...
   // checking db for previous user account
   const emailQuery = 'SELECT id FROM users WHERE email = ?'
-  db.query(emailQuery, [email], async (err, res2 => {
+  db.query(emailQuery, [email], async (err, res2) => {
     if (err) {
         // using 500 again cuz its not users fault per say...
         return res.status(500).json({
@@ -46,31 +46,43 @@ const registerUser = async (req, res) => {
             error_message: 'An account with this email already exists.'
         })
     }
-  }))
-
-
-  // hashing complexity...
-  const hashed_password = await bcrypt.hash(password, 8)
-
-  // inserting into db...
-  // ? -> prevents SQL injection attacks
-  const query = `
-    INSERT INTO users (first_name, last_name, email, hashed_password)
-    VALUES (?, ?, ?, ?)
-  `
-
-  // queries the database as specified...
-  db.query(query, [first_name, last_name, email, hashed_password], (err, result) => {
-    // error handling... to be expanded upon...
-    if (err) {
-      return res.status(500).json({ message: 'Something went wrong' })
-    }
-    // else success...
-    res.status(201).json({
-      success: true,
-      message: 'User has registered successfully'
-    })
   })
-}
 
+  // bringing into a try to ensure success/catch errors...
+  try {
+      // hashing complexity...
+    const hashed_password = await bcrypt.hash(password, 8)
+
+    // inserting into db...
+    // ? -> prevents SQL injection attacks
+    const query = `
+        INSERT INTO users (first_name, last_name, email, hashed_password)
+        VALUES (?, ?, ?, ?)
+    `
+    // queries the database as specified...
+    db.query(query, [first_name, last_name, email, hashed_password], (err, result) => {
+        // error handling... to be expanded upon...
+        if (err) {
+            return res.status(500).json({
+                error_code: 103,
+                error_title: 'Internal Query Error',
+                error_message: 'An internal error occured during database operations... please try again.'
+            })
+        }
+        // else success...
+        res.status(201).json({
+            success: true,
+            message: 'User has registered successfully'
+        })
+    }
+    )
+  } catch (error) {
+    return res.status(500).json({
+        error_code: 105,
+        error_title: 'Password Failure',
+        error_message: 'An internal error occured when securing your password!'
+    })
+  }
+}
+  
 module.exports = { registerUser }
